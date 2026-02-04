9a6ba414e090c0a9f50d1f9cd1c25ab4a9de8755
haatim10 <162633074+haatim10@users.noreply.github.com>
2026-02-05 04:17:31 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,23 +1,2 @@
 """PI-fMI-Fractional-PDE: Physics-Informed Neural Network for Fractional PDEs."""
-
-from .mesh import GradedMesh, compute_l1_coefficients
-from .model import PINN
-from .physics import fractional_derivative, compute_residual
-from .loss import PINNLoss
-from .dataset import CollocationDataset
-from .trainer import Trainer
-from .utils import exact_solution, compute_errors, plot_solution
-
-__all__ = [
-    "GradedMesh",
-    "compute_l1_coefficients",
-    "PINN",
-    "fractional_derivative",
-    "compute_residual",
-    "PINNLoss",
-    "CollocationDataset",
-    "Trainer",
-    "exact_solution",
-    "compute_errors",
-    "plot_solution",
-]
+__version__ = "1.0.0"
